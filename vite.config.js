d0bb538287cab913693befc362774eea588aeb09
zianlelieur-debug <zianlelieur@gmail.com>
2026-06-08 23:54:21 +0200

    fix: vite config corrected

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/Receptenboek2/',
   server: {
     port: 5173,
     open: true
